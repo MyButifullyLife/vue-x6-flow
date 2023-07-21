4cc8bfb9a01112df8e4116eb0de73f4f8f569e3b
Hoge <251275985@qq.com>
2023-07-22 07:51:58 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,14 +5,6 @@ Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/test",
-    name: "test",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "@/views/test"),
-  },
-  {
     path: "/dragDemo",
     name: "dragDemo",
     // route level code-splitting
