720c65f2597dbf3600b05f0d61e493a056f500ea
Hoser <251275985@qq.com>
2023-08-08 16:31:00 +0800

    增加demo2

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,12 @@ Vue.use(VueRouter);
 const routes = [
   {
     path: "/",
+    name: "Test",
+    component: () => import(/* webpackChunkName: "about" */ "@/views/test"),
+  },
+  {
+    path: "/dragDemo",
     name: "dragDemo",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "@/views/dragDemo"),
   },
   {
